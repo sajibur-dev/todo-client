ab6eb1932efd2171ef9bdafad25c6ce1206fa376
sajibur-dev <sajibur.dev@gmail.com>
2022-08-03 23:36:50 +0600

    navbar is done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,79 @@
-import React from 'react';
+import React from "react";
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
-    return (
-        <div>
-            <h1>navbar</h1>
+  return (
+    <div class="navbar bg-primary text-white text-bold">
+      <div class="navbar-start">
+      <Link  to="/" class="btn btn-ghost normal-case text-xl">Sigma Tannat</Link>
+        <div class="dropdown">
+          <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabindex="0"
+            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/todos">Todos</Link>
+            </li>
+          </ul>
         </div>
-    );
+      </div>
+      <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal p-0">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/todos">todos</Link>
+          </li>
+        </ul>
+      </div>
+      <div class="navbar-end">
+        <div class="dropdown dropdown-end text-black bg-primary">
+          <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+            <div class="w-10 rounded-full">
+              <img src="https://placeimg.com/80/80/people" alt="profile" />
+            </div>
+          </label>
+          <ul
+            tabindex="0"
+            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link to="#" class="justify-between">
+                Profile
+                <span class="badge">New</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="#">Settings</Link>
+            </li>
+            <li>
+              <Link to="#">Logout</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
